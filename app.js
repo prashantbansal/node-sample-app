8dbd1929aba4610d6428fd6cc861f226f2fa5dd2
Prashant Bansal <prashant.bansal@kaplan.com>
2021-07-30 11:54:59 -0400

    changes uptill lecture 97

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require('http'); //adding global module
+
+
+//const routes = require('./routes');
 //const http = require('./http'); //adding local file, if needed
-const https = require('https');
+//const https = require('https');
+//const fs = require('fs');
+
 
 // function requestListener(req, res){
 
@@ -10,8 +15,53 @@ const https = require('https');
 
 // });
 
-const server = http.createServer((req, res) => {
- console.log(req);
-});
+//const server = http.createServer((req, res) => {
 
-server.listen(3001);
+//});
+//console.log(routes.someText);
+//const server = http.createServer(routes.handler);
+//server.listen(3001);
+
+const path = require('path');
+const express = require('express');
+const bodyParser1 = require('body-parser');
+const expressHbs = require('express-handlebars');
+const app = express();
+
+// app.engine('hbs',expressHbs({
+//     layoutDir:'views/layouts', 
+//     defaultLayout: 'main-layout', 
+//     extname: 'hbs'}));
+// app.set('view engine','hbs');
+// app.set('views','views');
+
+//app.set('view engine','pug');
+//app.set('views','views');
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+const adminroutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+const routes_404 = require('./routes/404');
+
+app.use(bodyParser1.urlencoded({extended: false}));
+
+// app.use('/add-product', (req, res, next) => {
+//     console.log('in middleware');
+//     res.send('<form action="/product" method="POST"><input type = "text" name="title"><button type="Submit">Add Product</button></form>');
+//    });
+
+// app.post('/product', (req, res, next) =>{
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminroutes);
+app.use(shopRoutes);
+app.use(routes_404);
+
+
+
+app.listen(3001);
